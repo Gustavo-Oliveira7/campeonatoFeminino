9f72f71db50da05c594cd536f59ec9e4de9d4473
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-07-18 16:44:26 -0300

    model e migration teams criadas

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,29 @@
+import { DataTypes, Model, InferAttributes, InferCreationAttributes,
+  CreationOptional } from 'sequelize';
+import db from '.';
+
+class TeamsModel extends Model<InferAttributes<TeamsModel>,
+InferCreationAttributes<TeamsModel>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+TeamsModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'teams',
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+export default TeamsModel;

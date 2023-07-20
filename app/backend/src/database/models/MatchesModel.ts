cd05779287d8652d93feb6d34b9be4e2a78318a2
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-07-20 15:50:28 -0300

    migration e model para matches e ajustes nos nomes de arquivos

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,54 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class MatchesModel extends Model<InferAttributes<MatchesModel>,
+InferCreationAttributes<MatchesModel>> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+MatchesModel.init({
+  id: {
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+  },
+  homeTeamId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  awayTeamId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  inProgress: {
+    allowNull: false,
+    type: DataTypes.BOOLEAN,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+export default MatchesModel;
--- app/backend/src/database/models/UsersModel.ts
+++ app/backend/src/database/models/UsersModel.ts
@@ -6,10 +6,9 @@ import {
   CreationOptional,
 } from 'sequelize';
 import db from '.';
-// import OtherModel from './OtherModel ' ;
 
-class Users extends Model<InferAttributes<Users>,
-InferCreationAttributes<Users>> {
+class UsersModel extends Model<InferAttributes<UsersModel>,
+InferCreationAttributes<UsersModel>> {
   declare id: CreationOptional<number>;
   declare username: string;
   declare role: string;
@@ -17,7 +16,7 @@ InferCreationAttributes<Users>> {
   declare password: string;
 }
 
-Users.init({
+UsersModel.init({
   id: {
     type: DataTypes.INTEGER,
     allowNull: false,
@@ -47,15 +46,4 @@ Users.init({
   timestamps: false,
 });
 
-/**
-  * `Workaround` para aplicar as associations em TS:
-  * Associations 1:N devem ficar em uma das instâncias de modelo
-  * */
-
-// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
-// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-
-// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
-
-export default Users;
+export default UsersModel;

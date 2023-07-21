eac03c665cb52313203129628bc490d8fba86b5b
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-07-21 15:26:20 -0300

    adicionando o email regex

--- app/backend/src/middlewares/loginValidation.ts
+++ app/backend/src/middlewares/loginValidation.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 
 const msgResponse = 'All fields must be filled';
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 export const emailValidation = (req: Request, res: Response, next: NextFunction) => {
   const { email } = req.body;
   if (!email) {
     return res.status(400).json({ message: msgResponse });
   }
-  if (email.includes('exemplo')) {
+  if (!emailRegex.test(email)) {
     return res.status(401).json({ message: 'Invalid email or password' });
   }
   return next();

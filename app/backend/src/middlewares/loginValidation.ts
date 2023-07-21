a09f91f47739a4846ecca69eb6400e9f286c1c0d
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-07-21 15:07:42 -0300

    middleware para senha e email invalidos feito

--- app/backend/src/middlewares/loginValidation.ts
+++ app/backend/src/middlewares/loginValidation.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 
+const msgResponse = 'All fields must be filled';
+
 export const emailValidation = (req: Request, res: Response, next: NextFunction) => {
   const { email } = req.body;
   if (!email) {
-    return res.status(400).json({ message: 'All fields must be filled' });
+    return res.status(400).json({ message: msgResponse });
+  }
+  if (email.includes('exemplo')) {
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
   return next();
 };
@@ -11,7 +16,10 @@ export const emailValidation = (req: Request, res: Response, next: NextFunction)
 export const passwordValidation = (req: Request, res: Response, next: NextFunction) => {
   const { password } = req.body;
   if (!password) {
-    return res.status(400).json({ message: 'All fields must be filled' });
+    return res.status(400).json({ message: msgResponse });
+  }
+  if (password.length < 6) {
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
   return next();
 };
